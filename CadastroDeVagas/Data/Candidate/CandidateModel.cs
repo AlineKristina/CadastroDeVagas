b2f502901f56afcbfadbaa34e5a9925695b8f3c7
aline <aline@DESKTOP-BPKFLNI>
2024-11-28 22:22:58 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroDeVagas/Data/Candidate/CandidateModel.cs
@@ -0,0 +1,37 @@
+﻿namespace Data.Candidate
+{
+    public class CandidateModel
+    {
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Phone { get; set; }
+
+        public Document? Document { get; set; }
+
+        public Address? Address { get; set; }
+    }
+
+    public class Address
+    {
+        public string? City { get; set; }
+
+        public string? Region { get; set; }
+
+        public string? Country { get; set; }
+
+        public string? Street { get; set; }
+
+        public string? ZipCode { get; set; }
+
+        public string? State { get; set; }
+    }
+
+    public class Document
+    {
+        public string? Number { get; set; }
+
+        public string? Type { get; set; }
+    }
+}
---
+++ CadastroDeVagas/Data/Candidate/CandidateService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+namespace Data.Candidate
+{
+    public class CandidateService
+    {
+        private readonly IMongoCollection<CandidateModel> _candidates;
+
+        public CandidateService(IOptions<MongoDbSettings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            var database = client.GetDatabase(settings.Value.DatabaseName);
+            _candidates = database.GetCollection<CandidateModel>("candidates");
+        }
+
+        public async Task<List<CandidateModel>> GetAsync() =>
+            await _candidates.Find(candidate => true).ToListAsync();
+
+        public async Task<CandidateModel> GetAsync(string documentNumber) =>
+            await _candidates.Find(customer => customer.Document.Number == documentNumber).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(CandidateModel customer) =>
+            await _candidates.InsertOneAsync(customer);
+
+        public async Task UpdateAsync(string documentNumber, CandidateModel customerIn) =>
+            await _candidates.ReplaceOneAsync(customer => customer.Document.Number == documentNumber, customerIn);
+
+        public async Task RemoveAsync(string documentNumber) =>
+            await _candidates.DeleteOneAsync(customer => customer.Document.Number == documentNumber);
+    }
+}
